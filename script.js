de4e1eaf80bc4564899f61cf46e3111d145b59e6
sadmanador <gazitasnuva.nsu@gmail.com>
2022-09-27 23:03:20 +0600

    third task done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// task 1
+// task 1:
 function radianToDegree(radian) {
     const degree = 57.2958;
     return radian * degree;
@@ -6,4 +6,34 @@ function radianToDegree(radian) {
 
 console.log(radianToDegree())
 
-//task 2
+
+
+
+//task 2:
+function isJavaScriptFile (filename) {
+    let result = '';
+    const jsFile = ".js";
+    if ( filename.slice(-3) === jsFile){
+        result = true
+    } else {
+        result = false
+    }
+    return result;
+}
+
+console.log(isJavaScriptFile(""))
+
+
+//task 3:
+function oilPrice (diesel, petrol, octane) {
+    let a = 114;
+    let b = 130;
+    let c = 135;
+    return (diesel * a) + (petrol * b) + (octane * c);
+};
+
+
+//task 4:
+function publicBusFare () {
+    
+};
